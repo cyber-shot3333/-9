aedf74150f66103a513d7efb581839f61d658b0e
Илья Смирнов <cyber-shot3333@yandex.ru>
2021-11-29 16:35:58 +0300

    Добавьте файлы проекта.

---
+++ Задание 9/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            Console.WriteLine("Калькулятор C#\r");
+            Console.WriteLine("------------------------\n");
+            Console.WriteLine("Введи число №1 и нажми Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введи число №2 и нажми Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Выбери действия с числами №1 и №2 из следующего списка:");
+            Console.WriteLine("\ta - Сложить");
+            Console.WriteLine("\ts - Вычесть");
+            Console.WriteLine("\tm - Умножить");
+            Console.WriteLine("\td - Разделить");
+            Console.Write("Ваш выбор? ");
+
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Ваш результат: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Ваш результат: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Ваш результат: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Ваш результат: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            Console.Write("Нажми любую кнопку чтобы закрыть Калькулятор C#");
+            Console.ReadKey();
+        }
+    }
+}
